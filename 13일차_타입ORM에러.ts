75e5f48c82a22f57dc1644c1ce049d59e651b3b5
SANGGWONGIM <skkim@flexweb.io>
2022-06-30 12:41:42 +0900

    study done;

---
+++ 13일차_타입ORM에러.ts
@@ -0,0 +1,5 @@
+// Entity metadata for blahblah#blahblahs was not found
+
+// 새로운 모듈의 entity, resolver, graphql, service 등을 만들고 실행시켰을 때 발생한 에러입니다.
+// 이 에러는 해당 엔티티를 모듈에 Import 시키지 않아서 발생한 에러입니다.
+// [TypeOrmModule.forFeature()] 함으로써 에러를 해결할 수 있었습니다.
