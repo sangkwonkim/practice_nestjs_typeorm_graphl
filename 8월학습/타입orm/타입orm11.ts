978c99785559733aefc375153bfa6af0e2ed5ffe
SANGGWONGIM <sangkwon2212@gmail.com>
2022-08-16 14:02:01 +0900

    today done

--- 8월학습/타입orm/타입orm11.ts
+++ 8월학습/타입orm/타입orm11.ts
@@ -282,3 +282,334 @@
 //     @Column()
 //     name: string
 // }
+
+
+Entity Listeners and Subscribers
+
+모든 엔티티는 특정 엔티티 이벤트를 수신하는 커스텀 로직을 가진 method를 가질 수 있음
+어떤 이벤트를 수신할 지에 따라 그 방법들을 특별헌 데코레이터로 표시해야함
+
+listener 내부에서 데이터베이스를 호출하지 말고 대신 subscribers를 선태하셈
+
+
+@AfterLoad
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @AfterLoad로 표시할 수 있음
+그러면 타입orm은 엔티티가 쿼리빌더나 repository/manager의 find 메소드에 의해 로드될 때마다 호출할 것임
+
+@Entity()
+export class Post {
+    @AfterLoad()
+    updateCounters() {
+        if (this.likesCount === undefined) this.likesCount = 0
+    }
+} // view 카운트 올리기, 방문 기록 등 여부 확인 가능할 듯
+
+
+@BeforeInsert
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @BeforeInsert로 표시할 수 있음
+그러면 타입orm은 repository/manager save에 의해 엔티티에 insert되기 전에 호출할 것임 
+
+@Entity()
+export class Post {
+    @BeforeInsert()
+    updateDates() {
+        this.createdDate = new Date()
+    }
+}
+
+
+@AfterInsert
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @AfterInsert으로 표시할 수 있음
+그러면 타입orm은 repository/manager save에 의해서 엔티티에 insert된 후에 호출할 것임
+
+@Entity()
+export class Post {
+    @AfterInsert()
+    resetCounters() {
+        this.counters = 0
+    }
+}
+
+
+@BeforeUpdate
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @BeforeUpdate로 표시할 수 있음
+그러면 타입orm은 repository/manager save에 의해서 존재하는 엔티티를 업데이트하기 전에 호출할 것임
+그러나, 모델 내부의 정보가 바뀔 때에만 한해서 실행될 것임
+모델에서 아무것도 수정하지 않고 저장을 실행하면 @BeforeUpdate 및 @AfterUpdate가 실행되지 않음
+
+@Entity()
+export class Post {
+    @BeforeUpdate()
+    updateDates() {
+        this.updatedDate = new Date()
+    }
+}
+
+
+ @AfterUpdate
+ 엔티티에 임의의 이름을 가진 메서드를 정의하고 @AfterUpdate로 표시할 수 있음
+ 그러면 타입orm은 repository/manager save에 의해서 존재하는 엔티티를 업데이트한 후에 호출할 것임
+
+@Entity()
+export class Post {
+    @AfterUpdate()
+    updateCounters() {
+        this.counter = 0
+    }
+}
+
+
+@BeforeRemove
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @BeforeRemove로 표시할 수 있음
+그러면 타입orm은 repository/manager remove에 의해서 엔티티를 삭제하기 전에 호출할 것임
+
+
+@Entity()
+export class Post {
+    @BeforeRemove()
+    updateStatus() {
+        this.status = "removed"
+    }
+}
+
+
+@AfterRemove
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @AfterRemove로 표시할 수 있음
+그러면 타입orm은 repository/manager remove에 의해서 엔티티를 삭제한 후에 호출할 것임
+
+@Entity()
+export class Post {
+    @AfterRemove()
+    updateStatus() {
+        this.status = "removed"
+    }
+}
+
+
+@BeforeSoftRemove
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @BeforeSoftRemove로 표시할 수 있음
+그러면 타입orm은 repository/manager softRemove에 의해서 엔티티를 soft-delete하기 전에 호출할 것임
+
+@Entity()
+export class Post {
+    @BeforeSoftRemove()
+    updateStatus() {
+        this.status = "soft-removed"
+    }
+}
+
+
+@AfterSoftRemove
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @AfterSoftRemove로 표시할 수 있음
+그러면 타입orm은 repository/manager softRemove에 의해서 엔티티를 soft-delete한 후에 호출할 것임
+
+@Entity()
+export class Post {
+    @AfterSoftRemove()
+    updateStatus() {
+        this.status = "soft-removed"
+    }
+}
+
+
+@BeforeRecover
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @BeforeRecover로 표시할 수 있음
+그러면 타입orm은 repository/manager recover에 의해서 엔티티를 recover하기 전에 호출할 것임
+
+@Entity()
+export class Post {
+    @BeforeRecover()
+    updateStatus() {
+        this.status = "recovered"
+    }
+}
+
+
+@AfterRecover
+엔티티에 임의의 이름을 가진 메서드를 정의하고 @AfterRecover로 표시할 수 있음
+그러면 타입orm은 repository/manager recover에 의해서 엔티티를 recover한 후에 호출할 것임
+
+@Entity()
+export class Post {
+    @AfterSoftRemove()
+    updateStatus() {
+        this.status = "recovered"
+    }
+}
+
+
+
+What is a Subscriber
+특정 엔티티의 이벤트나, 엔티티 이벤트를 listen할 수 있는 event subscriber로 class를 표시함.
+QueryBuilder와 repository/manager methods를 사용함으로써 이벤트가 발생함
+
+@EventSubscriber()
+export class PostSubscriber implements EntitySubscriberInterface<Post> {
+    /**
+     * Indicates that this subscriber only listen to Post events.
+     */
+    listenTo() {
+        return Post
+    }
+
+    /**
+     * Called before post insertion.
+     */
+    beforeInsert(event: InsertEvent<Post>) {
+        console.log(`BEFORE POST INSERTED: `, event.entity)
+    }
+}
+
+EntitySubscriberInterface에서 모든 메소드를 실행할 수 있음. 아무 엔티티를 listen하려면 listenTo를 생략하고 다음을 사용하면 됨
+
+@EventSubscriber()
+export class PostSubscriber implements EntitySubscriberInterface {
+    /**
+     * Called after entity is loaded.
+     */
+    afterLoad(entity: any) {
+        console.log(`AFTER ENTITY LOADED: `, entity)
+    }
+
+    /**
+     * Called before post insertion.
+     */
+    beforeInsert(event: InsertEvent<any>) {
+        console.log(`BEFORE POST INSERTED: `, event.entity)
+    }
+
+    /**
+     * Called after entity insertion.
+     */
+    afterInsert(event: InsertEvent<any>) {
+        console.log(`AFTER ENTITY INSERTED: `, event.entity)
+    }
+
+    /**
+     * Called before entity update.
+     */
+    beforeUpdate(event: UpdateEvent<any>) {
+        console.log(`BEFORE ENTITY UPDATED: `, event.entity)
+    }
+
+    /**
+     * Called after entity update.
+     */
+    afterUpdate(event: UpdateEvent<any>) {
+        console.log(`AFTER ENTITY UPDATED: `, event.entity)
+    }
+
+    /**
+     * Called before entity removal.
+     */
+    beforeRemove(event: RemoveEvent<any>) {
+        console.log(
+            `BEFORE ENTITY WITH ID ${event.entityId} REMOVED: `,
+            event.entity,
+        )
+    }
+
+    /**
+     * Called after entity removal.
+     */
+    afterRemove(event: RemoveEvent<any>) {
+        console.log(
+            `AFTER ENTITY WITH ID ${event.entityId} REMOVED: `,
+            event.entity,
+        )
+    }
+
+    /**
+     * Called before entity removal.
+     */
+    beforeSoftRemove(event: SoftRemoveEvent<any>) {
+        console.log(
+            `BEFORE ENTITY WITH ID ${event.entityId} SOFT REMOVED: `,
+            event.entity,
+        )
+    }
+
+    /**
+     * Called after entity removal.
+     */
+    afterSoftRemove(event: SoftRemoveEvent<any>) {
+        console.log(
+            `AFTER ENTITY WITH ID ${event.entityId} SOFT REMOVED: `,
+            event.entity,
+        )
+    }
+
+    /**
+     * Called before entity removal.
+     */
+    beforeRecover(event: RecoverEvent<any>) {
+        console.log(
+            `BEFORE ENTITY WITH ID ${event.entityId} RECOVERED: `,
+            event.entity,
+        )
+    }
+
+    /**
+     * Called after entity removal.
+     */
+    afterRecover(event: RecoverEvent<any>) {
+        console.log(
+            `AFTER ENTITY WITH ID ${event.entityId} RECOVERED: `,
+            event.entity,
+        )
+    }
+
+    /**
+     * Called before transaction start.
+     */
+    beforeTransactionStart(event: TransactionStartEvent) {
+        console.log(`BEFORE TRANSACTION STARTED: `, event)
+    }
+
+    /**
+     * Called after transaction start.
+     */
+    afterTransactionStart(event: TransactionStartEvent) {
+        console.log(`AFTER TRANSACTION STARTED: `, event)
+    }
+
+    /**
+     * Called before transaction commit.
+     */
+    beforeTransactionCommit(event: TransactionCommitEvent) {
+        console.log(`BEFORE TRANSACTION COMMITTED: `, event)
+    }
+
+    /**
+     * Called after transaction commit.
+     */
+    afterTransactionCommit(event: TransactionCommitEvent) {
+        console.log(`AFTER TRANSACTION COMMITTED: `, event)
+    }
+
+    /**
+     * Called before transaction rollback.
+     */
+    beforeTransactionRollback(event: TransactionRollbackEvent) {
+        console.log(`BEFORE TRANSACTION ROLLBACK: `, event)
+    }
+
+    /**
+     * Called after transaction rollback.
+     */
+    afterTransactionRollback(event: TransactionRollbackEvent) {
+        console.log(`AFTER TRANSACTION ROLLBACK: `, event)
+    }
+}
+
+TypeORM이 subscribers를 로드하도록 DataSourceOptions에 subscribers 속성이 설정되어 있는지 확인해야함
+
+
+Event Object
+listenTo를 제외하고 모든 EntitySubscriberInterface 메소드는 다음과 같은 기본 속성을 가진 이벤트 객체에 전달됨
+
+dataSource: DataSource - 이벤트에서 사용할 DataSource
+queryRunner: QueryRunner - 이벤트 트랜잭션에서 사용할 쿼리러너
+manager: EntityManager - 이벤트 트랜잭션에서 사용할 EntityManager
+
+subscribe된 event listeners의 모든 데이터베이스 작업은 이벤트 객체의 queryRunner 또는 manager 인스턴스를 사용하여 수행해야 함
