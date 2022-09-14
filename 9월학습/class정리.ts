424097722320f2aced821f4148403e859aa90775
SANGGWONGIM <sangkwon2212@gmail.com>
2022-09-14 18:30:01 +0900

    today done

---
+++ 9월학습/class정리.ts
@@ -0,0 +1,25 @@
+// class DepositControl {
+//     log = null;
+//     constructor(log:BankLog) {
+//       this.log = log;
+//     }
+//      // 내가 예치한 내역이 없다면 뱅크 로그 객체를 만들어서 findBank에 push 후 save
+//     public firstDeposit() {
+//       this.someCalc();
+
+//     }
+//     public retryDeposit() {
+//       this.someCalc();
+      
+//     }
+//     private someCalc() {
+
+//     }
+//   }
+  
+//   const depositCtrl = new DepositControl();
+//   if (!findBankLog) {
+//     depositCtrl.firstDeposit();
+//   } else {
+//     depositCtrl.retryDeposit();
+//   }
